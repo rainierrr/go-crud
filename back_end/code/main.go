0e5d82233c0d3d457351bcfefe38195a7b2fc4e8
rainierrr <b1916320@planet.kanazawa-it.ac.jp>
2021-06-06 10:15:53 +0900

    DB接続

--- back_end/code/main.go
+++ back_end/code/main.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"net/http"
-	_ "github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gin-gonic/gin"
 )
+func gormConnect() *gorm.DB {
+  DBMS     := "mysql"
+  USER     := "go"
+  PASS     := "go"
+  PROTOCOL := "tcp(db:3306)"
+  DBNAME   := "go_db"
+
+  CONNECT := USER+":"+PASS+"@"+PROTOCOL+"/"+DBNAME
+  db,err := gorm.Open(DBMS, CONNECT)
+
+  if err != nil {
+    panic(err.Error())
+  }
+  return db
+}
 
 func main() {
+	db := gormConnect()
+  defer db.Close()
 	engine := gin.Default()
 	engine.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
